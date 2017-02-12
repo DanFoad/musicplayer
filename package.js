51308f768e5ed64ed1968f26c39f79da944c9f27
Dan Foad <danielfoad@gmail.com>
2017-02-12 23:48:54 +0000

    Initialise electron app

---
+++ package.js
@@ -0,0 +1,21 @@
+{
+    "name": "musicplayer",
+    "version": "1.0.0",
+    "description": "Music player and manager",
+    "main": "main.js",
+    "scripts": {
+        "start": "electron ."
+    },
+    "repository": "https://github.com/DanFoad/musicplayer",
+    "keywords": [
+        "music",
+        "player",
+        "manager",
+        "audio"
+    ],
+    "author": "Dan Foad",
+    "license": "MIT",
+    "devDependencies": {
+        "electron": "^1.4.1"
+    }
+}
