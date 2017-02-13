ea97d38b2353397644fe6d90d048c912375f0c2a
Dan Foad <danielfoad@gmail.com>
2017-02-13 00:08:57 +0000

    Switched to sass syntax from scss

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function() {
-    gulp.src('style/main.scss')
+    gulp.src('style/main.sass')
         .pipe(sass({style: 'expanded'}))
         .pipe(gulp.dest('style'));
 });
